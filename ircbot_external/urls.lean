09928a6d76734836f7545876a4f5cd59fcfb04a6
Siegmentation <siegmentationfault@yandex.ru>
2019-07-12 23:51:02 +0700

    fix

--- ircbot_external/urls.lean
+++ ircbot_external/urls.lean
@@ -21,8 +21,8 @@ def get_titles : irc_text → list irc_text
     { object := some object, type := message.privmsg,
       args := [subject], text := text }) :=
     list.filter_map (λ word, match run_string Url word with
-      | sum.inl url := some $ privmsg subject $ sformat! "URL found: {url}"
-      | sum.inr _ := none
+      | sum.inr url := some $ privmsg subject $ sformat! "URL found: {url}"
+      | sum.inl _ := none
     end) $ text.split (∈ delims)
 | _ := []
 
