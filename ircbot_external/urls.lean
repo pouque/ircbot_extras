5244d5c012e8632f9bf1083db066f4d61ea47c2a
Siegmentation <siegmentationfault@yandex.ru>
2019-07-14 23:00:00 +0700

    test

--- ircbot_external/urls.lean
+++ ircbot_external/urls.lean
@@ -23,10 +23,12 @@ def max_length := 30 * 1024
 
 def get_page_by_url (url : string) : io string := do
   --curl_proc ← io.proc.spawn
-  io.cmd
+  page ← io.cmd
     { cmd := "curl",
       args := [ "--max-time", to_string timeout, "--silent", "--no-keepalive", "--location", url ],
-      stdout := io.process.stdio.piped }
+      stdout := io.process.stdio.piped },
+  io.put_str_ln page,
+  pure page
   --page ← io.fs.read curl_proc.stdout max_length,
   --io.fs.close curl_proc.stdout,
   --pure $ buffer.to_string page
