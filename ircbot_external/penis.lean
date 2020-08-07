c70f22f2b9cd5634eefd54cdb58bd6fd189cb686
Siegmentation <siegmentationfault@yandex.ru>
2020-08-07 20:14:20 +0700

    fix

--- ircbot_external/penis.lean
+++ ircbot_external/penis.lean
@@ -4,7 +4,7 @@ open types support parser
 def string.hash (s : string) : nat :=
 list.foldl (+) 0 (char.to_nat <$> s.to_list)
 
-def list.get_by_hash {α : Type} [inhabited α] (xs : list α) (s : string) : α :=
+def list.get_by_hash {α : Type} [inhabited α] (s : string) (xs : list α) : α :=
 xs.get (s.hash % xs.length)
 
 namespace ircbot_external
@@ -31,9 +31,15 @@ def ideologies :=
    "eurasian", "communist", "fascist", "populist", "social democrat",
    "syndicalist", "nationalist", "schizophrenic"]
 
-def colours := ["nigger", "american nigger", "yellow-cheeked", "asian", "mexican", "bashkir", "buryat", "tatarstanian", "white"]
-def sizes := ["extremly fat", "abnormally fat", "very fat", "fat", "normal weight", "extremly thin", "thin", "skinny"]
-def look := ["very sexy", "sexy", "pretty", "normal looking", "unremarkable", "ugly", "freak", "retarded looking"]
+def colours :=
+  ["nigger", "american nigger", "yellow-cheeked", "asian", "mexican",
+   "bashkir", "buryat", "tatarstanian", "white"]
+def sizes :=
+  ["extremly fat", "abnormally fat", "very fat", "fat", "normal weight",
+   "extremly thin", "abnormally thin", "very thin", "thin", "skinny"]
+def look :=
+  ["very sexy", "sexy", "pretty", "normal looking", "unremarkable",
+   "ugly", "freak", "retarded looking"]
 def religious :=
   ["orthodox", "atheist", "catholic", "muslim",
    "judaist", "judaist", "agnostic", "pagan", "ukrop", "bulbash"]
@@ -46,26 +52,26 @@ def penis : bot_function :=
 router "penis" "Measures the penis." "\\penis <nick>" Word
   (λ msg nick,
     let length := penis.minimal_length + nick.hash % penis.maximal_length in
-    pure [ privmsg msg.subject $ sformat! "{nick} has {length} cm" ])
-  [ message.privmsg ]
+    pure [privmsg msg.subject $ sformat! "{nick} has {length} cm"])
+  [message.privmsg]
 
 def jew : bot_function :=
 router "jew" "Detect jews." "\\jew <nick>" Word
   (λ msg nick,
-    pure [ privmsg msg.subject
+    pure [privmsg msg.subject
       (if nat.to_bool (nick.hash % 2) then
         sformat! "{nick} is jew!"
-      else sformat! "{nick} is not jew.") ])
-  [ message.privmsg ]
+      else sformat! "{nick} is not jew.")])
+  [message.privmsg]
 
 def specs := [sizes, look, colours, genders, ideologies, religious]
 def get_profile (nick : string) :=
-string.intercalate " " $ (λ xs, list.get_by_hash xs nick) <$> specs
+string.intercalate " " (list.get_by_hash nick <$> specs)
 
 def profile : bot_function :=
 router "profile" "Return profile." "\\profile <nick>" Word
   (λ msg nick, let profile := get_profile nick in
-    pure [ privmsg msg.subject (sformat! "{nick}: {profile}") ])
-  [ message.privmsg ]
+    pure [privmsg msg.subject (sformat! "{nick}: {profile}")])
+  [message.privmsg]
 
 end ircbot_external
